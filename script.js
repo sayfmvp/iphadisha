ed2e09c2e7f0e8d12499b042d51a72a61c949bee
sayfmvp <rahmatullauly.s@gmail.com>
2025-11-25 22:49:41 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+document.getElementById("b0").addEventListener("click", function() {
+  alert("Байланыс бөліміне өтіңіз!");
+});
+document.getElementById("b1").addEventListener("click", function() {
+  alert("Байланыс бөліміне өтіңіз!");
+});
+document.getElementById("b2").addEventListener("click", function() {
+  alert("Байланыс бөліміне өтіңіз!");
+});
+document.getElementById("b3").addEventListener("click", function() {
+  alert("Байланыс бөліміне өтіңіз!");
+});
+document.getElementById("b4").addEventListener("click", function() {
+  alert("Байланыс бөліміне өтіңіз!");
+});
+document.getElementById("b5").addEventListener("click", function() {
+  alert("Байланыс бөліміне өтіңіз!");
+});
+document.getElementById("b6").addEventListener("click", function() {
+  alert("Байланыс бөліміне өтіңіз!");
+});
+document.getElementById("b7").addEventListener("click", function() {
+  alert("Байланыс бөліміне өтіңіз!");
+});
+document.getElementById("b8").addEventListener("click", function() {
+  alert("Байланыс бөліміне өтіңіз!");
+});
+document.getElementById("b9").addEventListener("click", function() {
+  alert("Байланыс бөліміне өтіңіз!");
+});
+document.getElementById("b10").addEventListener("click", function() {
+  alert("Байланыс бөліміне өтіңіз!");
+});
+document.getElementById("b11").addEventListener("click", function() {
+  alert("Байланыс бөліміне өтіңіз!");
+});
+document.getElementById("b12").addEventListener("click", function() {
+  alert("Байланыс бөліміне өтіңіз!");
+});
+document.getElementById("b13").addEventListener("click", function() {
+  alert("Байланыс бөліміне өтіңіз!");
+});
+document.getElementById("b14").addEventListener("click", function() {
+  alert("Байланыс бөліміне өтіңіз!");
+});
+document.getElementById("b15").addEventListener("click", function() {
+  alert("Байланыс бөліміне өтіңіз!");
+});
+
+
+
